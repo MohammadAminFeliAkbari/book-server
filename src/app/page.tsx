d9273e3b536fda7c046bfe2b188894157d4fd86f
MohammadAminFeliAkbari <mfly22317@gmail.com>
2025-03-28 19:02:40 +0330

    dibug code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Slider from '../components/Home/slider/Slider'
-
 export default function Home() {
   return (
     <div className='m-7'>
       <h1>لیست کتاب ها</h1>
       
-    {/* <Slider></Slider> */}
     </div>
   );
 }
