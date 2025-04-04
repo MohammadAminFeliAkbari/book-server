b0a335f6f0619fa7452ca6bb2152dc6a0568bcc7
MohammadAminFeliAkbari <mfly22317@gmail.com>
2025-04-04 10:50:05 +0330

    new header

--- src/components/Header/Drawer.jsx
+++ src/components/Header/Drawer.jsx
@@ -1,46 +1,72 @@
-import React, { useState } from 'react';
-import { Drawer as MUIDrawer, Button, List, ListItem, ListItemText } from '@mui/material';
+import React, { useState } from "react";
 
-const MyDrawer = ({open , setOpen}) => {
+import {
+  Drawer as MUIDrawer,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-        setOpen(open);
-    };
+const MyDrawer = ({ open, setOpen }) => {
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setOpen(open);
+  };
 
-    const list = () => (
-        <div
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
-            <List>
-                <ListItem button>
-                    <ListItemText primary="Item 1" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemText primary="Item 2" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemText primary="Item 3" />
-                </ListItem>
-            </List>
-        </div>
-    );
-
-    return (
-        <div>
-            <MUIDrawer
-                anchor='right'
-                open={open}
-                onClose={toggleDrawer(false)}
+  const list = () => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+      className="dark:bg-gray-500 h-full p-4"
+    >
+      <list>
+        <div className="h-full flex flex-col">
+          <h1 className="text-center p-3 text-gray-200">کتاب بان</h1>
+          <div className="border-b-1 text-gray-400 w-full"></div>
+          <button className="flex cursor-pointer py-2 dark:text-gray-300 gap-1 items-center justify-center text-[15px] ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="w-3 pb-1"
             >
-                {list()}
-            </MUIDrawer>
+              <path d="M15 3h4a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-4" />
+              <polyline points="10 17 15 12 10 7" />
+              <line x1="15" y1="12" x2="3" y2="12" />
+            </svg>
+            ورودیاعضویت
+          </button>
         </div>
-    );
+      </list>
+    </div>
+  );
+
+  return (
+    <div>
+      <MUIDrawer
+        anchor="right"
+        open={open}
+        onClose={toggleDrawer(false)}
+        sx={{ width: 250 }} // Set your desired width here
+        variant="temporary" // or "permanent" based on your need
+      >
+        {list()}
+      </MUIDrawer>
+    </div>
+  );
 };
 
-export default MyDrawer;  
+export default MyDrawer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,29 +4,17 @@ import React, { useState, useEffect, useContext } from "react";
 import { AppContext } from "../../../context/AppContext";
 import config from "../../config";
 import Drawer from "./Drawer";
+import { Button } from "@mui/material";
 
 export default function Header() {
   const [hidden, setHidden] = useState(false);
-  const [headerVisible, setHeaderVisible] = useState(false);
   const [username, setUsername] = useState("");
 
   const { refresh, access, setAccess, setRefresh } = useContext(AppContext);
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setHeaderVisible(true);
-    } else {
-      setHeaderVisible(false);
-    }
-  };
 
   useEffect(() => {
     setAccess(localStorage.getItem("access"));
     setRefresh(localStorage.getItem("refresh"));
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, []);
 
   useEffect(() => {
@@ -76,84 +64,45 @@ export default function Header() {
   }, [refresh, access]);
 
   return (
-    <>
-      <header className="dark:bg-gray-800 dark:text-gray-400 ">
-        <nav className="border-gray-200 p-4 bg-gray-100 bg-opacity-5 dark:bg-gray-800">
-          <div className="flex flex-wrap justify-between items-center">
-            <div className="flex items-center lg:order-2 justify-between">
-              {username ? (
-                <button className="cursor-pointer px-2">{username}</button>
-              ) : (
-                <div>
-                  <Link
-                    href={"/login"}
-                    onClick={() => setHidden(true)}
-                    className="border dark:border-gray-500 focus:ring-4 focus:ring-gray-500 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-400 transition-all hover:text-black focus:outline-none dark:focus:ring-gray-800"
-                  >
-                    ورود
-                  </Link>
-                  <Link
-                    href={"/signup"}
-                    onClick={() => setHidden(true)}
-                    className="bg-blue-700 text-white bg-primary-700 hover:bg-primary-800 focus:ring-2 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-                  >
-                    ثبت نام
-                  </Link>{" "}
-                </div>
-              )}
-              <button
-                data-collapse-toggle="mobile-menu-2"
-                type="button"
-                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden dark:hover:bg-gray-700 hover:bg-gray-200 dark:outline-none dark:hover:text-gray-500 mr-2 transition-all focus:outline-none focus:ring-1 focus:ring-gray-600 dark:text-gray-400"
-                aria-controls="mobile-menu-2"
-                aria-expanded={!hidden}
-                onClick={() => setHidden((prev) => !prev)}
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div
-              className={`hidden lg:block justify-between items-center w-full lg:w-auto lg:order-1`}
-              id="mobile-menu-2"
-            >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 gap-3">
-                <li>
-                  <button className="p-2 block py-2 pr-4 pl-3 w-full border-gray-100 lg:hover:bg-transparent lg:border-0 lg:p-0 lg:dark:hover:bg-transparent dark:border-gray-700">
-                    <Link
-                      href={"/"}
-                      onClick={() => setHidden(true)}
-                      className="p-2 flex justify-center items-center"
-                    >
-                      خانه
-                    </Link>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <Drawer open={hidden} setOpen={setHidden} />
-      </header>
-
-      <h1
-        className={`sticky ${
-          headerVisible ? "" : "hidden"
-        } top-0 bg-gray-100 dark:bg-gray-800 z-10 p-3 text-center border-t border-gray-400`}
+    <header className="flex gap-2 items-center w-full px-2 py-3 dark:bg-gray-800 dark:text-gray-400 ">
+      <button
+        onClick={() => setHidden((pre) => !pre)}
+        className="hover:bg-gray-600 rounded-full transition-all hover:text-gray-400"
       >
-        تخفیف ویژه
-      </h1>
-    </>
+        <svg
+          className="w-6 h-6 m-1"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      <div className="flex p-2 flex-1 relative items-center bg-gray-700 text-white rounded-md ">
+        <input
+          type="text"
+          className="text-[12px] outline-none flex-1 pl-8"
+          placeholder="چیزی بنویسید..."
+        />
+        <span className="text-white absolute left-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 m-2" // Tailwind classes to control size
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <circle cx="10" cy="10" r="7" strokeWidth="2" />
+            <line x1="16" y1="16" x2="21" y2="21" strokeWidth="2" />
+          </svg>
+        </span>
+      </div>
+      <Drawer open={hidden} setOpen={setHidden} />
+    </header>
   );
 }
