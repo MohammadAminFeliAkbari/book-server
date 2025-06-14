06b10f3e0664d08856cf53ab69d0493671cb6dee
mohammadAminFeliAkbari <mfly22317@gmail.com>
2025-06-14 17:17:40 +0330

    add domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['141.11.21.237'] // Configure allowed image domains
+    domains: ['ketabbaan.ir'] // Configure allowed image domains
   }
 }
 
